870add9900fcbf21d32988d9189dddd9d22726c6
brookd2026 <brookd2026@gmail.com>
2026-08-20 15:51:44 -0700

    chore: lock down ef core packages and configuration

---
+++ WebApi/Data/LibraryDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApi.Data
+{
+    public class LibraryDbContext : DbContext
+    {
+        public LibraryDbContext(DbContextOptions<LibraryDbContext> options) : base(options)
+        {
+        }
+
+        // Add your database tables (DbSets) here, for example:
+        // public DbSet<Book> Books { get; set; }
+    }
+}
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,4 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+using WebApi.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// If Render passes a postgres:// URL, translate it cleanly
+if (!string.IsNullOrEmpty(connectionString) && connectionString.StartsWith("postgres://"))
+{
+    var databaseUri = new Uri(connectionString);
+    var userInfo = databaseUri.UserInfo.Split(':');
+
+    connectionString = $"Host={databaseUri.Host};" +
+                       $"Port={databaseUri.Port};" +
+                       $"Database={databaseUri.LocalPath.TrimStart('/')};" +
+                       $"Username={userInfo[0]};" +
+                       $"Password={userInfo[1]};" +
+                       $"Pooling=true;" +
+                       $"SSL Mode=Require;" +
+                       $"Trust Server Certificate=true;";
+}
+
+builder.Services.AddDbContext<LibraryDbContext>(options =>
+    options.UseNpgsql(connectionString));
+
 var app = builder.Build();
 
 // Render assigns a dynamic port via the PORT environment variable
