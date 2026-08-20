8cb06dcf76ce7fbcf556a0f643c98d03a6fdf726
brookd2026 <brookd2026@gmail.com>
2026-08-20 16:39:53 -0700

    remove connection string split

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -7,20 +7,20 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 // If Render passes a postgres:// URL, translate it cleanly
-if (!string.IsNullOrEmpty(connectionString) && connectionString.StartsWith("postgres://"))
-{
-    var databaseUri = new Uri(connectionString);
-    var userInfo = databaseUri.UserInfo.Split(':');
+//if (!string.IsNullOrEmpty(connectionString) && connectionString.StartsWith("postgres://"))
+//{
+//    var databaseUri = new Uri(connectionString);
+//    var userInfo = databaseUri.UserInfo.Split(':');
 
-    connectionString = $"Host={databaseUri.Host};" +
-                       $"Port={databaseUri.Port};" +
-                       $"Database={databaseUri.LocalPath.TrimStart('/')};" +
-                       $"Username={userInfo[0]};" +
-                       $"Password={userInfo[1]};" +
-                       $"Pooling=true;" +
-                       $"SSL Mode=Require;" +
-                       $"Trust Server Certificate=true;";
-}
+//    connectionString = $"Host={databaseUri.Host};" +
+//                       $"Port={databaseUri.Port};" +
+//                       $"Database={databaseUri.LocalPath.TrimStart('/')};" +
+//                       $"Username={userInfo[0]};" +
+//                       $"Password={userInfo[1]};" +
+//                       $"Pooling=true;" +
+//                       $"SSL Mode=Require;" +
+//                       $"Trust Server Certificate=true;";
+//}
 
 builder.Services.AddDbContext<LibraryDbContext>(options =>
     options.UseNpgsql(connectionString));
